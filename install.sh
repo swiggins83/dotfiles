508691f0fedb2fefae827f7a3ee1f96b9e6c85e6
swiggins83 <stevenwiggins83@gmail.com>
2013-08-08 10:15:12 -0400

    Update install script a lil bit

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-sudo apt-get install zsh vim awesome awesome-extra &&
+sudo apt-get install zsh vim xterm awesome awesome-extra &&
 cp .zshrc ~
 cp .vimrc ~
 cp -r .vim ~
 cp -r .zsh ~
+cp -r extras ~
 sudo cp rc.lua /etc/xdg/awesome/
