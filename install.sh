f5ab556683acc9e54619f84e130255916e0377c2
Steven Wiggins <stevenwiggins83@gmail.com>
2015-05-20 13:49:12 -0400

    Cleanup vim plugins and add some

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install zsh vim-gnome xterm git awesome awesome-extra
-
 dotfiles=$(pwd)
 cd ~
 ln -s $dotfiles/.zshrc .
@@ -11,29 +5,23 @@ ln -s $dotfiles/.vimrc .
 ln -s $dotfiles/.vim .
 ln -s $dotfiles/.zsh .
 ln -s $dotfiles/.Xresources .
-#rc.lua - mkdir .config/awesome??
 cd -
 
 cd ..
 git clone https://github.com/zsh-users/zsh-syntax-highlighting
 git clone https://github.com/olivierverdier/zsh-git-prompt
 git clone https://github.com/zsh-users/zsh-history-substring-search
-git clone https://github.com/funayman/PySpotifyInfo
 cd -
 
 vim
 cd ~/.vim/bundle
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/jistr/vim-nerdtree-tabs.git
-git clone https://github.com/Lokaltog/vim-easymotion.git
-git clone https://github.com/vim-scripts/closetag.vim
 git clone https://github.com/bronson/vim-trailing-whitespace
 git clone https://github.com/akhaku/vim-java-unused-imports
 git clone https://github.com/kien/rainbow_parentheses.vim
-#git clone https://github.com/vim-scripts/VimClojure
-#git clone https://github.com/mattn/emmet-vim.git
 git clone git://github.com/mkitt/tabline.vim.git
 git clone https://github.com/Valloric/MatchTagAlways
+git clone https://github.com/gorodinskiy/vim-coloresque
+git clone https://github.com/fatih/vim-go
 cd -
 
 cp herald.vim ~/.vim/colors/
