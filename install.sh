7103fecb015ac15d534a0a9418276904ba616d58
Steven <stevenwiggins83@gmail.com>
2017-05-10 14:26:10 -0400

    Modernize the installboi script

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
+echo "Installing the goodness..."
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y \
+  zsh awesome awesome-extra vim nodejs npm atom chromium-browser haskell-platform \
+  alsa-utils emacs24 xterm
+sudo apt autoremove
+npm install -g yarn
+npm install -g gulp
+cabal install cabal cabal-install
+
+echo "Changing shells..."
+chsh -s /bin/zsh
+
+echo "Linking files..."
 dotfiles=$(pwd)
 cd ~
 ln -s $dotfiles/.zshrc .
@@ -5,28 +20,27 @@ ln -s $dotfiles/.vimrc .
 ln -s $dotfiles/.vim .
 ln -s $dotfiles/.zsh .
 ln -s $dotfiles/.Xresources .
-cd -
 
-cd ..
+mkdir .vimundo
+mkdir .config/awesome
+cd .config/awesome
+ln -s $dotfiles/rc.lua .
+
+cd $dotfiles/..
 git clone https://github.com/zsh-users/zsh-syntax-highlighting
 git clone https://github.com/olivierverdier/zsh-git-prompt
 git clone https://github.com/zsh-users/zsh-history-substring-search
-cd -
 
 vim
 cd ~/.vim/bundle
 git clone https://github.com/bronson/vim-trailing-whitespace
-git clone https://github.com/akhaku/vim-java-unused-imports
 git clone https://github.com/kien/rainbow_parentheses.vim
 git clone git://github.com/mkitt/tabline.vim.git
 git clone https://github.com/Valloric/MatchTagAlways
 git clone https://github.com/gorodinskiy/vim-coloresque
 git clone https://github.com/fatih/vim-go
 git clone https://github.com/terryma/vim-expand-region
-cd -
+
+cd $dotfiles
 
 cp herald.vim ~/.vim/colors/
-
-cd
-mkdir .vimundo
-cd -
